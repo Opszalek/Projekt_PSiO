d8201f4380977f5a6cad12fc6b364cbc08e94395
Mikołaj Antczak <66114616+Mikolaj-Antczak@users.noreply.github.com>
2020-06-03 14:47:47 +0300

    Better use of TextureManager

--- main.cpp
+++ main.cpp
@@ -29,60 +29,49 @@ void ruch(sf::Sprite &rectangle,float time)
 
 int main()
 {
+    // Loading all textures in TextureManager
+    TextureManager::loadTexture("texture_guy","Tekstury/head.png")
+    TextureManager::loadTexture("texture_hp", "Tekstury/hp100.png")
+    TextureManager::loadTexture("texture_wall", "Tekstury/trawa.png")
+    
+    // Create main Window
     sf::RenderWindow window(sf::VideoMode(800, 600), "Giereczka");
 
+    // Create event
     sf::Event event;
+    
+    // Create clock
     sf::Clock klok;
-
-
- ////////////////
-    sf::Texture texture_guy;
-    if (!texture_guy.loadFromFile("Tekstury/head.png")) {
-        return 1;
-    }
-
+    
+    // Create player character
     sf::Sprite guy;
-    guy.setTexture(texture_guy);
+    guy.setTexture(TextureManager::getTexture("texture_guy"));
     guy.setTextureRect(sf::IntRect(0, 0, 45, 90)); //left, top, width, height
     guy.setPosition(500.0, 400.0);
 
 /////////////////
 
-    sf::Texture texture_hp;
-    if (!texture_hp.loadFromFile("Tekstury/hp100.png")) {
-        return 1;
-    }
-
     sf::Sprite boy_hp;
-    boy_hp.setTexture(texture_hp);
+    boy_hp.setTexture(TextureManager::getTexture("texture_hp"));
     boy_hp.setTextureRect(sf::IntRect(0, 0, 100,20)); //left, top, width, height
     boy_hp.setPosition(275.0, 275.0);
+    
 /////////////////
-
-
-
-
-    sf::Texture texture_wall;
-    if (!texture_wall.loadFromFile("Tekstury/trawa.png")) {
-        return 1;
-    }
-    texture_wall.setRepeated(true);
+    
+    TextureManager::getTexture("texture_wall")->setRepeated(true);
 
     sf::Sprite wall;
-    wall.setTexture(texture_wall);
+    wall.setTexture(TextureManager::getTexture("texture_wall"));
     wall.setScale(1, 1);
     wall.setTextureRect(sf::IntRect(0, 0, 800, 600));
+    
 ///////////
-    sf::Texture boy_texture;
-    if (!boy_texture.loadFromFile("Tekstury/head1.png")) {
-        return 1;
-    }
 
+    // Create second player character
     character boy;
-    boy.setTexture(boy_texture);
+    boy.setTexture(TextureManager::getTexture("texture_guy"));
     boy.setTextureRect(sf::IntRect(0,0,45,90));
     boy.setPosition(300.0, 300.0);
-
 
     sf::Vector2f p_size(10.0, 10.0);
     Bullet pocisk(p_size);
@@ -91,7 +80,6 @@ int main()
     //Obiekty
 
     std::vector<Bullet> bulletVec;
-
 
     sf::Clock delay;
 
@@ -102,7 +90,6 @@ int main()
                 window.close();
         }
 
-
         sf::Time elapsed = klok.restart();
         float czas = elapsed.asSeconds();
         ruch(guy,czas);
@@ -110,21 +97,17 @@ int main()
 
         window.clear(sf::Color::White);
 
-
         ////
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
             boy.zdrowie(0.0001, boy_hp, texture_hp);
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::M) && (delay.getElapsedTime().asMilliseconds()>150)) {
-
             Bullet newbullet(sf::Vector2f(10, 10));
             newbullet.setPos(sf::Vector2f(boy.getPosition().x, boy.getPosition().y));
             bulletVec.emplace_back(newbullet);
             delay.restart();
         }
-    ////
-
 
         window.draw(wall);
         window.draw(guy);
@@ -132,15 +115,12 @@ int main()
         window.draw(boy);
 
         for (auto &r : bulletVec) {
-
             r.draw(window);
             r.fire(czas,boy.direction());
 
         }
-
-      window.display();
-
-
+        
+        window.display();
   }
   return 0;
 }
